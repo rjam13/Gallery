ee2b678852ed3ccaa302b4eae7758b648f3861fb
Rey Jairus Marasigan <reyjairusmarasigan@gmail.com>
2021-09-14 10:40:19 -0400

    Initial Commit

---
+++ Gallery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Gallery
+//
+//  Created by Rey Jairus Marasigan on 9/14/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
